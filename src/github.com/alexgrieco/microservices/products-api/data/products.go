101955957c043a0baf1c761a62be4797049ae25d
Alex Grieco <agrieco992@gmail.com>
2020-03-04 21:31:29 -0500

    lesson-4

--- src/github.com/alexgrieco/microservices/products-api/data/products.go
+++ src/github.com/alexgrieco/microservices/products-api/data/products.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io"
 	"time"
+	"fmt"
 )
 
 // Product is a struct that includes all the necessary information for the product
@@ -18,6 +19,11 @@ type Product struct {
 	DeletedOn   string  `json:"-"`
 }
 
+func (p *Product) FromJSON(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(p)
+}
+
 // Products is a subset of type Product with only user-important info
 type Products []*Product
 
@@ -30,6 +36,39 @@ func (p *Products) ToJSON(w io.Writer) error {
 // GetProducts returns list of type Product
 func GetProducts() Products {
 	return productList
+}
+
+
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+func getNextID() int {
+	lp := productList[len(productList) - 1]
+	return lp.ID + 1
+}
+
+func UpdateProduct(id int, p*Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+	p.ID = id
+	productList[pos] = p
+
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
 }
 
 var productList = []*Product{
